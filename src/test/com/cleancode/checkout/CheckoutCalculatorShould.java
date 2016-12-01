5be3eb04f9b8c12e02c109af960fac41c293555f
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-01 22:22:11 +0530

    RED - Added testcase : return_total_price_as_30_for_each_unit_of_item_A_and_item_B

--- src/test/com/cleancode/checkout/CheckoutCalculatorShould.java
+++ src/test/com/cleancode/checkout/CheckoutCalculatorShould.java
@@ -3,6 +3,9 @@ package com.cleancode.checkout;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CheckoutCalculatorShould {
     @Test
     public void return_total_price_as_10_for_one_unit_of_item_A() {
@@ -14,5 +17,12 @@ public class CheckoutCalculatorShould {
     public void return_total_price_as_20_for_one_unit_of_item_B() {
         CheckOutCalculator checkOutCalculator =  new CheckOutCalculator();
         Assert.assertEquals(20, checkOutCalculator.calculatePrice("B"));
+    }
+
+    @Test
+    public void return_total_price_as_30_for_each_unit_of_item_A_and_item_B () {
+        CheckOutCalculator checkOutCalculator =  new CheckOutCalculator();
+        List<String> itemsList = Arrays.asList("A","B");
+        Assert.assertEquals(30, checkOutCalculator.calculatePrice(itemsList));
     }
 }
