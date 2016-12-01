fb71adee415f97ff83ae13c8bf86267baf8b0f00
Rohitash Tulyani <rtulyani@sapient.com>
2016-12-02 00:59:44 +0530

    GREEN - Implemented code for handling discount for Item A

--- src/main/com/cleancode/checkout/CheckOutCalculator.java
+++ src/main/com/cleancode/checkout/CheckOutCalculator.java
@@ -15,8 +15,22 @@ public class CheckOutCalculator {
 
     public int calculatePrice(List<String> itemsList) {
         int sum = 0;
+        int quantityForDiscountForItemA = 3;
+        int priceAfterDiscountForItemA = 25;
+        Map<String, Integer> quantityCounterMap = new HashMap<String,Integer>();
         for (String item: itemsList) {
-            sum += priceList.getOrDefault(item, 0);
+            if(quantityCounterMap.containsKey(item)) {
+                quantityCounterMap.put(item, quantityCounterMap.get(item)+1);
+            } else {
+                quantityCounterMap.put(item,1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : quantityCounterMap.entrySet()) {
+            if ("A".equals(entry.getKey()) && quantityForDiscountForItemA == entry.getValue()) {
+                sum += priceAfterDiscountForItemA;
+            } else {
+                sum += priceList.getOrDefault(entry.getKey(), 0) * entry.getValue();
+            }
         }
         return sum;
     }
